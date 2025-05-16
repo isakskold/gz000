5f7e98bc0ad3696a3de611ed602d526e23a12965
Isak Sköld <yhfe23.skolis@folkuniversitetet.nu>
2025-05-16 19:37:52 +0200

    auth fix maybe

---
+++ netlify/functions/auth.js
@@ -0,0 +1,40 @@
+const fetch = require("node-fetch");
+
+exports.handler = async function (event) {
+  const params = new URLSearchParams(event.queryStringParameters);
+  const code = params.get("code");
+
+  if (!code) {
+    return {
+      statusCode: 400,
+      body: "Missing code parameter",
+    };
+  }
+
+  const clientId = process.env.GITHUB_CLIENT_ID;
+  const clientSecret = process.env.GITHUB_CLIENT_SECRET;
+
+  const response = await fetch(`https://github.com/login/oauth/access_token`, {
+    method: "POST",
+    headers: { Accept: "application/json" },
+    body: new URLSearchParams({
+      client_id: clientId,
+      client_secret: clientSecret,
+      code,
+    }),
+  });
+
+  const data = await response.json();
+
+  if (data.error) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify(data),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ token: data.access_token }),
+  };
+};
